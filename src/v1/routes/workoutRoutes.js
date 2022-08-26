08116b4acf3f17248163270a3041df9b07584add
wrom <willy.maykros.romero@gmail.com>
2022-08-26 03:51:32 -0500

    apicache on routes

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const apicache = require("apicache");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
+const cache = apicache.middleware;
 
 // workout
+
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/", workoutController.getAllWorkouts);
 
