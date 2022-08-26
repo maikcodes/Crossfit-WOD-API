9908a9f1cd2ba15ae9d06936cdbc36aab5221a82
wrom <willy.maykros.romero@gmail.com>
2022-08-26 04:26:05 -0500

    apicache on index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const v1MemberRouter = require("./v1/routes/memberRoutes");
 
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 4000;
 
 // Settings
 app.use(bodyParser.json());
-
-// testing
-app.get("/", (req, res) => {
-  res.send("<h1> Setting up Crosssit WOD API </h1>");
-});
+app.use(cache("2 minutes"));
 
 app.use("/api/v1/workouts", v1WorkoutRouter);
 app.use("/api/v1/members", v1MemberRouter);
